47a42f7fd231484b1d38f71cde3499ddc0f1935f
immartinsk <immartinsk@gmail.com>
2021-03-13 21:50:47 +0100

    added multer and sharp for image processing

--- src/server.js
+++ src/server.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const sharp = require('sharp');
+const fs = require('fs');
 const app = express();
 const port = 8000;
 
@@ -6,11 +10,53 @@ const chalk = require('chalk');
 const log = console.log;
 const prefix = chalk.bold.blue('[KREEDZEU]');
 
+const savedFiles = 'uploads/';
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, savedFiles);
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype !== 'image/jpeg' ||
+    path.extname(file.originalname) !== '.jpg'
+  )
+    cb(new Error('Wrong filetype'));
+  cb(null, true);
+};
+
+const upload = multer({
+  dest: savedFiles,
+  storage: storage,
+  fileFilter: fileFilter,
+});
+
 app.use(express.static('src/public'));
 
-app.get('/example', (req, res) => {
-  log(prefix, chalk.bold(`Example console message`));
-  res.writeHead(200).end('Hello World');
+app.post('/upload', upload.single('file'), (req, res) => {
+  try {
+    res.status(200);
+    log(prefix, chalk.blue(`File uploaded successfully`));
+    const file = req.file;
+    const removeExtension = file.originalname.split('.').shift();
+    const extension = '.webp';
+    sharp(file.path)
+      .resize(640, 360)
+      .toFile(`${removeExtension}${extension}`, (err, info) => {
+        if (err) return new Error(err);
+        console.log(info);
+      });
+  } catch (error) {
+    if (error) {
+      log(prefix, chalk.red.bold('There has been a problem.'));
+      throw new Error(error);
+    }
+  }
 });
 
 app.listen(port, log(chalk.bold(`Server started on http://localhost:${port}`)));
